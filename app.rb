175072d358208b14d5014b9aaa9ba487459ec13d
Alyssa Kim <alyssa.kim@gmail.com>
2018-05-23 11:42:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,10 +4,11 @@ class App < Sinatra::Base
   # Write your code here!
   get 'reversename/:name' do
     @name = params[:name]
-    "#{@name}".reverse
+    @name.reverse
   end
 
   get 'square/:number' do
+    @number = params[:number].to_i 
   end 
 
 end
